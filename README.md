14bbf834091a5de24000a893650bd2768007525b
mofm <emre.eryilmaz@piesso.com>
2024-01-27 02:51:39 +0300

    rewritten whole script

--- README.md
+++ README.md
@@ -4,44 +4,29 @@ Kernel configuration copying, compile, grub2 and systemd-boot updating, old kern
 
 ## Usage
 
-* First, clone this repository and change directory:
-
-```sh
-git clone https://github.com/mofm/kernel-build.git
-cd kernel-build
-```
-
-* Change script permissons:
-
-```sh
-chmod +x build-kernel.sh
-```
-
 * If you are using **systemd-boot** you should first learn rootfs disk **UUID** or **LVM** partitions and then add it to the script:
 
 
 ```sh
-UUID="dfc588c0-edd4-8543-a3fe-d7d49bd8f141"
+ROOT_UUID="dfc577c0-edd4-8543-a3fe-d7d49bd8f141"
 ```
 
-**Optional:** *You can add specific kernel parameters to ROOTFLAGS.* Example:
+**Optional:** *You can add specific kernel parameters to CMDLINE.* Example:
 
 ```
-ROOTFLAGS="ro init=/usr/lib/systemd/systemd rootfstype=ext4 quiet"
+CMDLINE="ro init=/usr/lib/systemd/systemd rootfstype=ext4 quiet"
 ```
 
-Now you execute the script like so
+```
+Usage: ./build-kernel.sh: [-h] [-c] [-i] [-l] [-s] [-r] [-a] 
+  -h  Show this help message
+  -c  Configure and compile kernel
+  -i  Build, Install kernel and initramfs
+  -l  Update boot system(grub or systemd-boot)
+  -s  Sign kernel with secure boot key and cert
+  -r  Remove old kernel(s) and initramfs(exclude last 2 kernel(s))
+  -a  All of the above (execute all options)
+```
 
- ```sh
- ./build-kernel.sh systemd-boot
- ```
-
- * If you are using **grub2**:
- 
- ```sh
- ./build-kernel.sh grub2
- ```
-
----
- **NOTE**: This script only cleans the previous kernel.
----
+NOTE: I have rewritten the script. If you are using the old version, you should update it.
+changed and rewritten whole
--- build-kernel.sh
+++ build-kernel.sh
@@ -1,170 +1,331 @@
 #!/bin/bash
+# This script is used to build the kernel and initramfs on Gentoo Linux.
 
 #Required 
 #The UUID of your disk. systemd-boot needs it. be careful!
 # Note: if using LVM, this should be the LVM partition.
-UUID=""
+ROOT_UUID=""
 
 #Optional
 # Any rootflags you wish to set. For example, mine are currently
 # "subvol=@ quiet splash intel_pstate=enable".
-ROOTFLAGS=""
+CMDLINE=""
+
+# Secure boot key and cert
+# If you want to sign your kernel with a secure boot key, set this to the path of your key.
+# if you added your key and cert to in the make.conf, you can leave it blank.
+SECUREBOOT_SIGN_KEY=""
+SECUREBOOT_SIGN_CERT=""
+
+# Path to kernel source directory: Default is "/usr/src/linux"
+KERNEL_DIR="/usr/src/linux"
+
+die() {
+  printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2  # bold red
+  exit 1
+}
+
+einfo() {
+  printf '\033[1;32mINFO:\033[0m %s\n' "$@" >&2  # bold green
+}
+
+ewarn() {
+  printf '\033[1;33mWARN:\033[0m %s\n' "$@" >&2  # bold yellow
+}
+
+# getopts options
+while getopts ":hcilsra" opt; do
+  case $opt in
+    h)
+      printf "Usage: %s: [-h] [-c] [-i] [-l] [-s] [-r] [-a] \n" "$0"
+      printf "  -h  Show this help message\n"
+      printf "  -c  Configure and compile kernel\n"
+      printf "  -i  Build, Install kernel and initramfs\n"
+      printf "  -l  Update boot system(grub or systemd-boot)\n"
+      printf "  -s  Sign kernel with secure boot key and cert\n"
+      printf "  -r  Remove old kernel(s) and initramfs(exclude last 2 kernel(s))\n"
+      printf "  -a  All of the above(execute all options)\n"
+      exit 0
+      ;;
+    c)
+      COMPILE_KERNEL="true"
+      ;;
+    i)
+      INSTALL_KERNEL="true"
+      ;;
+    l)
+      UPDATE_BOOT_SYSTEM="true"
+      ;;
+    s)
+      SIGN_KERNEL="true"
+      ;;
+    r)
+      REMOVE_OLD_KERNEL="true"
+      ;;
+    a)
+      COMPILE_KERNEL="true"
+      INSTALL_KERNEL="true"
+      UPDATE_BOOT_SYSTEM="true"
+      SIGN_KERNEL="true"
+      REMOVE_OLD_KERNEL="true"
+      ;;
+    \?)
+      die "Invalid option: -$OPTARG"
+      ;;
+  esac
+done
+
+#check getopts options
+if [ "$#" -eq 0 ]; then
+  die "No options specified. See -h for help."
+fi
+
+pushd_quiet() {
+    pushd "$1" &>/dev/null || die "Failed to enter $1."
+}
+
+popd_quiet() {
+    popd &>/dev/null || die "Failed to leave $1."
+}
 
 build_initramfs() {
 	printf "\n"
-	printf "Starting to building initramfs..please wait...\n"
+	einfo "Starting to building initramfs..please wait..."
 	is_installed "sys-kernel/genkernel"
-	vendor=$(cat /proc/cpuinfo | grep -m 1 vendor_id | awk '{ printf "%s\n", $3 }')
-	if [ $vendor == "GenuineIntel" ]; then
+  vendor=$(grep -m 1 vendor_id /proc/cpuinfo | awk '{ printf "%s\n", $3 }')
+	if [ "$vendor" == "GenuineIntel" ]; then
 		is_installed "sys-firmware/intel-microcode"
-		genkernel --install --no-ramdisk-modules --lvm --microcode initramfs
+		genkernel --install --no-ramdisk-modules --lvm --microcode initramfs || die "Failed to build initramfs"
 	else
-		genkernel --install --no-ramdisk-modules --lvm initramfs
+		genkernel --install --no-ramdisk-modules --lvm initramfs || die "Failed to build initramfs"
 	fi
+	einfo "Initramfs built successfully."
 }
 
 is_installed() {
-	package=$1
-    package_test=$(equery -q list "$package")
-    if [ -z ${package_test} ]; then
-		install_package "$package"
-    fi
+  package=$1
+  package_test=$(equery -q list "$package")
+  if [ -z "${package_test}" ]; then
+    install_package "$package"
+  fi
 }
 
 install_package() {
-	emerge -q $@
+	emerge -q "$@"
 	if [ $? -eq 1 ]; then
 		etc-update --automode -5
-		emerge -q $@
+		emerge -q "$@" || die "Failed to install $1"
 	fi
 	env-update && source /etc/profile
 }
 
-compile_kernel() {
-	printf "\n"
-	printf "Cleaning directory... \n"
-	cd /usr/src/linux
-	make clean
-	printf "\n"
-	printf "Copying old config... \n"
-	modprobe configs
-	if [ $? -gt 0 ]; then
-		printf "Error: failed to probe configs kernel module, must not be enabled - try another method. \n"
-	fi
-	zcat /proc/config.gz > /usr/src/linux/.config
-	if [ $? -gt 0 ]; then
-		CONFIG=$(find /boot -maxdepth 1 -name 'config-*' -type f -not -name '*.old' -print0 | xargs -r -0 ls -t | tail -1)
-		cp ${CONFIG} /usr/src/linux/.config
-	else
-		printf "Error: failed to copy kernel config file to /usr/src/linux/.config - try another method. \n"
-		exit 1
-	fi
-	printf "\n"
-	printf "Starting to build kernel.. please wait... \n"
-	source /etc/portage/make.conf
-	printf "\n"
-        printf "Configure Kernel\n"
-        make olddefconfig
-	printf "\n"
-	printf "Compiling bzImage\n"
-	make ARCH=$(uname -m) ${MAKEOPTS} bzImage
-	if [ $? -eq 0 ]; then
-		printf "\n"
-		printf "Compiling modules\n"
-		make ARCH=$(uname -m) ${MAKEOPTS} modules
-		printf "\n"
-		printf "compiling finished. installing kernel and modules\n"
-		make INSTALL_MOD_STRIP=1 modules_install
-		make install
-	fi
+
+compile_kernel () {
+  printf "\n"
+  einfo "Cleaning up kernel source tree..."
+  pushd_quiet "$KERNEL_DIR"
+  make -s mrproper || die "Failed to clean kernel source tree"
+  printf "\n"
+  einfo "Copying kernel config..."
+  # try to multiple methods to copy the kernel config
+  if [ -f /etc/kernels/kernel-config-"$(uname -m)" ]; then
+    cp /etc/kernels/kernel-config-"$(uname -m)" .config || die "Failed to copy kernel config"
+  elif [ -f /proc/config.gz ]; then
+    zcat /proc/config.gz > .config || die "Failed to copy kernel config"
+  elif [ -f /boot/config-"$(uname -r)" ]; then
+    cp /boot/config-"$(uname -r)" .config || die "Failed to copy kernel config"
+  else
+    die "Failed to find kernel config"
+  fi
+  printf "\n"
+  einfo "Starting to compile kernel...please wait..."
+  if [ -f /etc/portage/make.conf ]; then
+    source /etc/portage/make.conf
+  fi
+  printf "\n"
+  einfo "Configuring kernel..."
+  make -s olddefconfig || die "Failed to configure kernel"
+  printf "\n"
+  einfo "Compiling kernel..."
+  make -s ARCH="$(uname -m)" "${MAKEOPTS}"  bzImage || die "Failed to compile kernel"
+  printf "\n"
+  einfo "Compiling kernel modules..."
+  make -s ARCH="$(uname -m)" "${MAKEOPTS}" modules || die "Failed to compile kernel modules"
+  printf "\n"
+  einfo "Kernel and Modules compiled successfully."
+  popd_quiet "$KERNEL_DIR"
+}
+
+#install kernel and modules
+install_kernel() {
+  printf "\n"
+  einfo "Installing kernel and modules..."
+  pushd_quiet "$KERNEL_DIR"
+  make -s INSTALL_MOD_STRIP=1 modules_install || die "Failed to install kernel modules"
+  make -s install || die "Failed to install kernel"
+  printf "\n"
+  einfo "Kernel and modules installed successfully."
+  popd_quiet "$KERNEL_DIR"
+}
+
+# this function find installed kernel(s) in /boot and create bash array
+find_kernels() {
+  local KERNELS=()
+  readarray -t FOUND_KERNEL < <(find /boot -maxdepth 1 -name 'vmlinuz-*' -type f -not -name '*.old' -print0 | xargs -r -0 ls -t | sort -n)
+  # remove basename from kernel path and add to array
+  for i in "${FOUND_KERNEL[@]}"; do
+    KERNEL=("$(basename "$i")")
+    KERNELS+=("${KERNEL:8}")
+  done
+  # return array
+  echo "${KERNELS[@]}"
+}
+
+# this function create systemd-boot entry given kernel version
+create_systemd_entry() {
+  VERSION=$1
+  FILENAME=$2
+  cat <<EOF > /boot/loader/entries/"${FILENAME}".conf
+title Gentoo Linux ${VERSION}
+linux /vmlinuz-${VERSION}
+initrd /initramfs-${VERSION}.img
+options root=UUID=${ROOT_UUID} ${CMDLINE}
+EOF
 }
 
 update_grub() {
-	printf "\n"
-	printf "Updating grub config\n"
-	printf "\n"
-	isBootMounted=$(mount | grep /boot)
-	if [ -z "${isBootMounted}" ]; then
-		printf "Warning: /boot is not mounted - mount before attempting to proceed with GRUB update. \n"
-		exit 1
-	elif [ ! -d /boot/grub/ ]; then
-		printf "\n"
-		printf "Error: /boot/grub/ directory does not exist. Please emerge sys-boot/grub:2 \n"
-		exit 1
-	fi
-	printf "\n"
-	grub-mkconfig -o /boot/grub/grub.cfg
-	if [ $? -gt 0 ]; then
-		printf "Error: didn't create grub.cfg. \n"
-		exit 1
-	fi
+  printf "\n"
+  einfo "Updating grub..."
+  # check if boot partition is mounted and
+  # grub is installed
+  if ! mount | grep -qs "/boot" ; then
+    ewarn "Boot partition is not mounted or separate boot partition not found"
+  elif [ ! -d "/boot/grub" ]; then
+    die "/boot/grub directory not found. Please emerge sys-boot/grub:2"
+  else
+    grub-mkconfig -o /boot/grub/grub.cfg || die "Failed to update grub"
+  fi
 }
 
-update_systemd() {
-	printf "\n"
-	printf "Updating systemd-boot\n"
-	isBootMounted=$(mount | grep /boot)
-	if [ -z "${isBootMounted}" ]; then
-		printf "Warning: /boot is not mounted - mount before attempting to proceed with systemd-boot update. \n"
-		exit 1
-	elif [ ! -d /boot/loader/ ]; then
-		printf "Error: /boot/loader/ directory does not exist. Please install systemd-boot loader with 'bootctl install'. \n"
-		exit 1
-	fi
-	equery -N -C uses systemd | grep '^ + + gnuefi'
-	if [ $? -gt 0 ]; then
-		printf "Error: systemd doesn't support efi. please enable gnuefi useflag and re-emerge. \n"
-		exit 1
-	fi
-
-	# Our kernels.
-	KERNELS=()
-	FIND="find /boot -maxdepth 1 -name 'vmlinuz-*' -type f -not -name '*.old' -print0 | sort -Vrz"
-	while IFS= read -r -u3 -d $'\0' LINE; do
-		KERNEL=$(basename "${LINE}")
-		KERNELS+=("${KERNEL:8}")
-	done 3< <(eval "${FIND}")
-
-	# There has to be at least one kernel.
-	if [ ${#KERNELS[@]} -lt 1 ]; then
-		echo -e "\e[2msystemd-boot\e[0m \e[1;31mNo kernels found.\e[0m"
-		exit 1
-	fi
-
-	# Copy the latest kernel files to a consistent place so we can
-	# keep using the same loader configuration.
-	LATEST="${KERNELS[@]:0:1}"
-	echo -e "\e[2msystemd-boot\e[0m \e[1;32m${LATEST}\e[0m"
-	cat << EOF > /boot/loader/entries/gentoo.conf
-title   Gentoo Linux
-linux   /vmlinuz-${LATEST}
-initrd  /initramfs-${LATEST}.img
-options root=UUID=${UUID} rw ${ROOTFLAGS}
-EOF
-
-	# Copy any legacy kernels over too, but maintain their version-
-	# based names to avoid collisions.
-	if [ ${#KERNELS[@]} -gt 1 ]; then
-		LEGACY=("${KERNELS[@]:1}")
-		for VERSION in "${LEGACY[@]}"; do
-		    echo -e "\e[2msystemd-boot\e[0m \e[1;32m${VERSION}\e[0m"
-		    cat << EOF > /boot/loader/entries/gentoo-${VERSION}.conf
-title   Gentoo Linux ${VERSION}
-linux   /vmlinuz-${VERSION}
-initrd  /initramfs-${VERSION}.img
-options root=UUID=${UUID} rw ${ROOTFLAGS}
-EOF
-		done
-	fi
-
-	printf "\n"
-	printf "Updating systemd-boot firmware \n"
-	bootctl update
-
-	# Success!
-	echo -e "\e[2m---\e[0m"
+update_systemd_boot() {
+  printf "\n"
+  einfo "Updating systemd-boot..."
+  # check if boot partition is mounted and
+  # systemd-boot is installed
+  if ! mount | grep -qs "/boot" ; then
+    ewarn "Boot partition is not mounted or separate boot partition not found"
+  elif [ ! -d "/boot/loader" ]; then
+    die "/boot/loader directory not found. Please emerge sys-boot/systemd-boot"
+  fi
+  # check if >=systemd-254 is installed with boot use flag(older versions is required to install with gnu-efi use flag)
+  if ! equery -N -C uses systemd | grep -e '^ + [-+] boot' -e '^ + [-+] gnuefi' -qs; then
+    die "sys-boot/systemd must be installed with boot use flag"
+  fi
+  # create local array from find_kernels function
+  local KERNELS=()
+  IFS=" " read -r -a KERNELS <<< "$(find_kernels)"; unset IFS
+  # check if kernel(s) found
+  if [ "${#KERNELS[@]}" -eq 0 ]; then
+    die "No kernel(s) found in /boot"
+  fi
+  # create systemd-boot entry for latest kernel
+  LATEST_KERNEL="${KERNELS[-1]}"
+  einfo "Creating systemd-boot entry for ${LATEST_KERNEL}"
+  create_systemd_entry "${LATEST_KERNEL}" "gentoo" || die "Failed to create systemd-boot entry"
+  # create previous kernel entries
+  for i in "${!KERNELS[@]}"; do
+    if [ "$i" -gt 0 ]; then
+      PREV_KERNEL="${KERNELS[$i-1]}"
+      einfo "Creating systemd-boot entry for ${PREV_KERNEL}"
+      create_systemd_entry "${PREV_KERNEL}" "gentoo-${PREV_KERNEL}" || die "Failed to create systemd-boot entry"
+    fi
+  done
+  printf "\n"
+  einfo "Updating systemd-boot firmware ..."
+  # disabled below line because 'bootctl update' exit with error code 1
+  # But It works fine. So, I dont check exit code.
+#  bootctl update || die "Failed to update systemd-boot firmware"
+  bootctl -q update
+  einfo "Systemd-boot updated successfully."
 }
 
+# detect and update boot system: systemd-boot or grub
+update_boot_system() {
+  printf "\n"
+  einfo "Detecting boot system..."
+  # check if boot partition is mounted
+  if ! mount | grep -qs "/boot" ; then
+    ewarn "Boot partition is not mounted or separate boot partition not found"
+  fi
+  # check if systemd-boot is installed
+  if [ -d "/boot/loader" ]; then
+    update_systemd_boot
+  elif [ -d "/boot/grub" ]; then
+    update_grub
+  else
+    die "Boot system not found"
+  fi
+}
+
+# sign kernel with secure boot key and cert
+sign_kernel () {
+  printf "\n"
+  einfo "Signing kernel with secure boot key and cert..."
+  # check if secure boot key and cert is set
+  if [ -n "$SECUREBOOT_SIGN_KEY" ] && [ -n "$SECUREBOOT_SIGN_CERT" ]; then
+    einfo "Secure boot key and cert is set in build-kernel.sh"
+  elif [ -f /etc/portage/make.conf ]; then
+    source /etc/portage/make.conf
+    if [ -n "$SECUREBOOT_SIGN_KEY" ] && [ -n "$SECUREBOOT_SIGN_CERT" ]; then
+      einfo "Secure boot key and cert is set in make.conf"
+    else
+      die "Secure boot key and cert is not set"
+    fi
+  fi
+  # check if sbsign is installed
+  is_installed "app-crypt/sbsigntools"
+  # create local array from find_kernels function
+  local KERNELS=()
+  IFS=" " read -r -a KERNELS <<< "$(find_kernels)"; unset IFS
+  # check if kernel(s) found
+  if [ "${#KERNELS[@]}" -eq 0 ]; then
+    die "No kernel(s) found in /boot"
+  fi
+  # sign kernel(s)
+  for i in "${!KERNELS[@]}"; do
+    einfo "Signing kernel ${KERNELS[$i]}"
+    sbsign --key "$SECUREBOOT_SIGN_KEY" --cert "$SECUREBOOT_SIGN_CERT" --output "/boot/vmlinuz-${KERNELS[$i]}" "/boot/vmlinuz-${KERNELS[$i]}" || die "Failed to sign kernel"
+  done
+}
+
+# remove old kernel(s) and initramfs exclude last 2 kernel(s)
+remove_old_kernel() {
+  # create local array from find_kernels function
+  local KERNELS=()
+  IFS=" " read -r -a KERNELS <<< "$(find_kernels)"; unset IFS
+  # check if kernel(s) found
+  if [ "${#KERNELS[@]}" -eq 0 ]; then
+    die "No kernel(s) found in /boot"
+  fi
+  # remove old kernel(s) exclude last 2 kernel(s)
+  for i in "${!KERNELS[@]}"; do
+    if [ "$i" -lt $((${#KERNELS[@]}-2)) ]; then
+      einfo "Removing old kernel ${KERNELS[$i]} from /boot"
+      rm -f "/boot/vmlinuz-${KERNELS[$i]}" || die "Failed to remove kernel"
+      rm -f "/boot/System.map-${KERNELS[$i]}" || die "Failed to remove System.map"
+      rm -f "/boot/config-${KERNELS[$i]}" || die "Failed to remove config"
+      rm -f "/boot/initramfs-${KERNELS[$i]}.img" || die "Failed to remove initramfs"
+      einfo "Removing old kernel ${KERNELS[$i]} from /lib/modules"
+      rm -rf "/lib/modules/${KERNELS[$i]}" || die "Failed to remove kernel modules"
+      einfo "Removing old kernel ${KERNELS[$i]} from /usr/src"
+      rm -rf "/usr/src/linux-${KERNELS[$i]}" || die "Failed to remove kernel source"
+      einfo "Removing old kernel systemd-boot entry"
+      rm -f "/boot/loader/entries/gentoo-${KERNELS[$i]}.conf" || die "Failed to remove systemd-boot entry"
+    else
+      einfo "Keeping kernel ${KERNELS[$i]}"
+    fi
+  done
+}
 
 # control_c()
 # Trap Ctrl-C for a quick and clean exit when necessary
@@ -179,7 +340,7 @@ trap control_c SIGINT
 banner() {
 	printf "\n"
 	printf "============================================================= \n"
-	printf "Gentoo kernel build \n"
+	printf "Gentoo Linux kernel build \n"
 	printf "https://github.com/mofm/kernel-build \n"
 	printf "============================================================= \n"
 	printf "\n"
@@ -188,49 +349,33 @@ banner() {
 
 banner
 
-# check root privileges and
-# do nothing if both versions are the same
-new_kernel_ver=$(readlink /usr/src/linux | sed 's/^linux-//');
-current_kernel_ver=$(uname -r | sed 's/-x86_64$//');
+# check if user is root
 if [ "$EUID" -ne 0 ]; then
-	printf "\n"
-	printf "Warning: Please run this script as root privileges. \n"
-	exit 1
-elif [ ${new_kernel_ver} == ${current_kernel_ver} ]; then
-	printf "\n"
-	printf "Warning: eselect kernel was unset, please set it. \n"
-	exit 1
+  die "Please run as root"
 fi
 
-if [ "$1" == "systemd-boot" ]; then
-	if [ -z "$UUID" ]; then
-		printf "Error: Please add rootfs UUID. aborted. \n"
-		exit 1
-	else
-		compile_kernel
-		build_initramfs
-		#rebuild modules
-		emerge -1 @module-rebuild
-		update_systemd
-	fi
-elif [ "$1" == "grub2" ]; then
-	compile_kernel
-	build_initramfs
-	#rebuild modules
-	emerge -1 @module-rebuild
-	update_grub
-else
-	printf "Error: Please select loader: systemd-boot or grub2 \n"
-	exit 1
+# compile kernel
+if [ "$COMPILE_KERNEL" == "true" ]; then
+  compile_kernel
 fi
 
-# clean old kernel
-printf "\n"
-printf "Cleaning old kernel, modules and files. \n"
-rm -rf /boot/{config,vmlinuz,System.map}-${current_kernel_ver}
-rm -rf /boot/initramfs-${current_kernel_ver}.img
-rm -rf /lib/modules/${current_kernel_ver}
+# install kernel and modules and build initramfs
+if [ "$INSTALL_KERNEL" == "true" ]; then
+  install_kernel
+  build_initramfs
+fi
 
-printf "\n" 
-printf "Complete! \n"
+# sign kernel with secure boot key and cert
+if [ "$SIGN_KERNEL" == "true" ]; then
+  sign_kernel
+fi
 
+# update boot system
+if [ "$UPDATE_BOOT_SYSTEM" == "true" ]; then
+  update_boot_system
+fi
+
+# remove old kernel(s) and initramfs
+if [ "$REMOVE_OLD_KERNEL" == "true" ]; then
+  remove_old_kernel
+fi
